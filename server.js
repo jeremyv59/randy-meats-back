2f521a8cd8cf583dc9bb050e70ecb9e924859b01
jérémy vaneste <jeremy.vaneste@free.fr>
2022-08-10 18:15:01 +0200

    implement code to launch the server with express

---
+++ server.js
@@ -0,0 +1,15 @@
+// Get and store express package in variable
+const express = require("express");
+
+// Use express to our application
+const app = express();
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.json({ "recettes": ["RecipeOne", "RecipeTwo", "RecipeThree"] });
+});
+
+app.listen(port, () => {
+  console.log(`Server launched on port ${port}`);
+});
