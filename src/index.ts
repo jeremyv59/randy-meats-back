4c7bea0fac69c25a2b4cb775c693fbc89d8e97fc
jérémy vaneste <jeremy.vaneste@free.fr>
2022-08-22 12:21:01 +0200

    add exceptions in main file index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { config } from "./config";
+import { ExceptionsHandler } from "./middlewares/exception.handler";
+import { UnknownRoutesHandler } from "./middlewares/unknownRoutes.handler";
 
 // Get and store express package in variable
 const express = require("express");
@@ -11,6 +13,12 @@ const port = 5000;
 app.get("/", (req: any, res: any) => {
   res.json({ recettes: ["RecipeOne", "RecipeTwo", "RecipeThree"] });
 });
+
+// Retourne une erreur pour les routes non définies
+app.all("*", UnknownRoutesHandler);
+
+// Gestion des erreurs
+app.use(ExceptionsHandler);
 
 app.listen(config.API_PORT, () => {
   console.log(`Server launched on port ${port}`);
